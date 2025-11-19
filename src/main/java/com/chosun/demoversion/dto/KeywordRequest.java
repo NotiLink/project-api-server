862e9b622c9162e67fff1ef30dd3269d33f2dfaa
sanghyeon42 <songv67@gmail.com>
2025-11-19 22:22:08 +0900

    added jsonproperty

--- src/main/java/com/chosun/demoversion/dto/KeywordRequest.java
+++ src/main/java/com/chosun/demoversion/dto/KeywordRequest.java
@@ -1,5 +1,6 @@
 package com.chosun.demoversion.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 public class KeywordRequest {
     private String targetUrl;
     private String keyword;
+    @JsonProperty("isNotifyEnabled")
     private boolean isNotifyEnabled; // 알림 켜기/끄기
     private String notifyChannel; // 알림 채널
 }
